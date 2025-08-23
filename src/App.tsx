e169cc189ca4b016cea6e960d3d1ae166a9cabb8
Obioha Gerald <zuziobioha@gmail.com>
2025-08-23 14:55:25 +0100

    feat: 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,63 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./Components/Layout"
-import Home from "./Components/Home"
-import About from "./Components/About"
-import Contact from "./Components/Contact"
-import AllLubricants from "./Lubricants/AllLubricants"
-import BikeOil from "./Lubricants/BikeOil"
-import PetrolEngineOil from "./Lubricants/PetrolEngineOil"
-import DieselEngine from "./Lubricants/DieselEngine"
-import TransmissionOil from "./Lubricants/TransmissionOil"
-import HeavyDutyEngineOil from "./Lubricants/HeavyDutyEngineOil"
-import CasstekPrem from "./Lubricants/DataOnLub/CasstekPrem"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import AllLubricants from "./Lubricants/AllLubricants";
+import BikeOil from "./Lubricants/BikeOil";
+import PetrolEngineOil from "./Lubricants/PetrolEngineOil";
+import DieselEngine from "./Lubricants/DieselEngine";
+import TransmissionOil from "./Lubricants/TransmissionOil";
+import HeavyDutyEngineOil from "./Lubricants/HeavyDutyEngineOil";
+import CasstekPrem from "./Lubricants/DataOnLub/CasstekPrem";
 
 const App = () => {
   return (
-   <BrowserRouter>
-     <div className="min-h-screen bg-gray-100">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about-us" element={<About/>} />
-          <Route path="contact" element={<Contact />} />
-          {/* Lubricants routes */}
-          <Route path="lubricants" element={<AllLubricants />} />
-          <Route path="lubricants/bike-oils" element={<BikeOil />} />
-          <Route path="lubricants/petrol-engine-oil" element={<PetrolEngineOil />} />
-          <Route path="lubricants/diesel-engine-oil" element={<DieselEngine />} /> 
-          <Route path="lubricants/transmission-oil" element={<TransmissionOil />} />
-          <Route path="lubricants/heavy-duty-engine-oil" element={<HeavyDutyEngineOil />} />
-          {/* Lubricants Data */}
-          <Route path="lubricants/dataonlub/cassket-prem" element={<CasstekPrem />} />
-        </Route>
-      </Routes>
-         </div>
-    </BrowserRouter>
-     
-  )
-}
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about-us" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            {/* Lubricants routes */}
+            <Route path="lubricants" element={<AllLubricants />} />
+            <Route path="lubricants/bike-oils" element={<BikeOil />} />
+            <Route
+              path="lubricants/petrol-engine-oil"
+              element={<PetrolEngineOil />}
+            />
+            <Route
+              path="lubricants/diesel-engine-oil"
+              element={<DieselEngine />}
+            />
+            <Route
+              path="lubricants/transmission-oil"
+              element={<TransmissionOil />}
+            />
+            <Route
+              path="lubricants/heavy-duty-engine-oil"
+              element={<HeavyDutyEngineOil />}
+            />
+            {/* Lubricants Data */}
+            <Route
+              path="lubricants/dataonlub/cassket-prem"
+              element={<CasstekPrem />}
+            />
 
-export default App
+            <Route
+              path="*"
+              element={
+                <h1 className="text-center mt-20 text-3xl">
+                  404 - Page Not Found
+                </h1>
+              }
+            />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
